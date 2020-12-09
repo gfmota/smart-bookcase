a999878f2111bb5d502c537433e33c30f559f90c
Gabriel Mota <gabrielfmota134@gmail.com>
2020-12-09 15:55:24 -0300

    Add main prototype
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- manager.py
+++ manager.py
@@ -1,3 +1,6 @@
+import os.path
+import csv
+
 class Book:
   def __init__(self, title, author, genre, reading_date, pages):
     self.title = title
@@ -15,3 +18,22 @@ class Bookcase:
 
   def search_by_parameter(self, parameter, value):
     return filter(lambda book: getattr(book, parameter) == value, self.books)
+
+def main():
+  printf("Welcome to your SmartBookcase! What would you want today?\n")
+  while True:
+    command = input("Type 1 if you want to add a new book to your bookcase.\nType 2 if you want to see all of your bookcase.\nType 3 if you want to search by something in your bookcase.\nType 4 if you want to quit.")
+    if command == 1:
+      printf("You want to add a new book\n")
+    elif command == 2:
+      printf("You want to see all of your books\n")
+    elif command == 3:
+      printf("You want to search something in your bookcase\n")
+    elif commad == 4:
+      break;
+    else:
+      printf("Please, type a valid command.\n")
+
+
+if __name__ == ‘__main__’:
+    main()
