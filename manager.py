a47fe1e1686c032ae4ded32504c3a1c3a29bf9dd
Gabriel Mota <gabrielfmota134@gmail.com>
2020-12-14 14:42:01 -0300

    Fix some syntax errors
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- manager.py
+++ manager.py
@@ -20,20 +20,22 @@ class Bookcase:
     return filter(lambda book: getattr(book, parameter) == value, self.books)
 
 def main():
-  printf("Welcome to your SmartBookcase! What would you want today?\n")
+  print("Welcome to your SmartBookcase! What would you want today?\n")
   while True:
     command = input("Type 1 if you want to add a new book to your bookcase.\nType 2 if you want to see all of your bookcase.\nType 3 if you want to search by something in your bookcase.\nType 4 if you want to quit.")
-    if command == 1:
-      printf("You want to add a new book\n")
+    if command == 0:
+      print("You want to view again\n")
+    elif command == 1:
+      print("You want to add a new book\n")
     elif command == 2:
-      printf("You want to see all of your books\n")
+      print("You want to see all of your books\n")
     elif command == 3:
-      printf("You want to search something in your bookcase\n")
+      print("You want to search something in your bookcase\n")
     elif commad == 4:
       break;
     else:
-      printf("Please, type a valid command.\n")
+      print("Please, type a valid command.\n")
 
 
-if __name__ == ‘__main__’:
+if __name__ == "__main__":
     main()
