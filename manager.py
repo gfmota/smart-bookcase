ea4c43ffa15abfb497c0441362c198d9ce8902f1
Gabriel Mota <gabrielfmota134@gmail.com>
2020-12-14 15:07:24 -0300

    Add a separated file for classes
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- manager.py
+++ manager.py
@@ -1,23 +1,6 @@
+import classes
 import os.path
 import csv
-
-class Book:
-  def __init__(self, title, author, genre, reading_date, pages):
-    self.title = title
-    self.author = author
-    self.genre = genre
-    self.reading_date = reading_date
-    self.pages = pages
-  
-class Bookcase:
-  def __init__(self, books):
-    self.books = books
-  
-  def add_new_book(self, new_book):
-    self.books.push(new_book)
-
-  def search_by_parameter(self, parameter, value):
-    return filter(lambda book: getattr(book, parameter) == value, self.books)
 
 def main():
   print("Welcome to your SmartBookcase! What would you want today?\n")
