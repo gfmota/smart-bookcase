93665630ff4ee40e8f4bb11ed0bec2f692ebe15f
Gabriel Mota <gabrielfmota134@gmail.com>
2020-12-14 16:54:57 -0300

    Add creating new csv or opening and converting it into bookcase
    Signed-off-by: Gabriel Mota <gabrielfmota134@gmail.com>

--- manager.py
+++ manager.py
@@ -1,23 +1,42 @@
-import classes
-import os.path
+from classes import Book
+from classes import Bookcase
 import csv
+from datetime import datetime
+
+def open_bookcase():
+  try:
+    with open('bookcase.csv', 'r') as bookcase_file:
+      csv_reader = csv.reader(bookcase_file)
+      next(csv_reader)
+      books = []
+      for line in csv_reader:
+        books.append(Book(line[0], line[1], line[2], line[3], int(line[4])))
+
+  except FileNotFoundError:
+    with open('bookcase.csv', 'w') as bookcase_file:
+      csv_writer = csv.writer(bookcase_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+      csv_writer.writerow(['title','author','genre','reading_date','pages'])
+    books = []
+
+  return Bookcase(books)
 
 def main():
   print("Welcome to your SmartBookcase! What would you want today?\n")
+
+  bookcase = open_bookcase()
+
   while True:
-    command = input("Type 1 if you want to add a new book to your bookcase.\nType 2 if you want to see all of your bookcase.\nType 3 if you want to search by something in your bookcase.\nType 4 if you want to quit.")
-    if command == 0:
-      print("You want to view again\n")
-    elif command == 1:
-      print("You want to add a new book\n")
+    command = int(input("Type 1 if you want to add a new book to your bookcase.\nType 2 if you want to see all of your bookcase.\nType 3 if you want to search by something in your bookcase.\nType 4 if you want to QUIT.\n"))
+    if command == 1:
+      print("You want to add a new book")
     elif command == 2:
-      print("You want to see all of your books\n")
+      print("You want to see all of your books")
     elif command == 3:
-      print("You want to search something in your bookcase\n")
-    elif commad == 4:
+      print("You want to search something in your bookcase")
+    elif command == 4:
       break;
-    else:
-      print("Please, type a valid command.\n")
+    elif command != 0:
+      print("Please, type a valid command.")
 
 
 if __name__ == "__main__":
